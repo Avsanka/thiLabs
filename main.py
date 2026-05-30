b377e159ec6b8a1132269ddb5b9bef21a2c3c83e
Avsanka <nikita555534@gmail.com>
2026-05-30 15:19:04 +0600

    dbtest

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import io
 import json
 from flask import Flask, render_template, request, send_file, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select, func
 import pymysql
-from models import Users, Reviews
+import models as cm
 
 
 app = Flask(__name__)
@@ -18,15 +19,72 @@ db = SQLAlchemy(app)
 def index():
     return render_template("index.html")
 
-@app.route('/dbtest')
-def dbtest():
+@app.route('/dbtest1')
+def dbtest1():
     with app.app_context():
         result = db.session.query(
-            Users.name, Users.surname, Users.email, Reviews.content
-            ).join(
-            Reviews, Users.user_ID == Reviews.ID_User
-            ).all()
-    return render_template("dbtest.html", list = result)
+            cm.Session.session_datetime, cm.Session.price, cm.Film.title, cm.Hall.hall_name
+        ).join(
+            cm.Film, cm.Session.ID_film == cm.Film.film_id
+        ).join(
+            cm.Hall, cm.Session.ID_hall == cm.Hall.hall_id
+        ).order_by(cm.Session.session_datetime).all()
+    return render_template("dbtest.html", test1=result)
+
+# @app.route('/dbtest2')
+# def dbtest2():
+#     with app.app_context():
+#         result = db.session.query(
+#             cm.Film.title, cm.Film.year, cm.Film.director, cm.Film.duration_mins, cm.Film.rating,
+#             cm.Country.country_name, cm.Genre.genre_name
+#         ).join(
+#             cm.t_film_genre, cm.t_film_genre.ID_film == cm.Film.film_id
+#         ).join(
+#             cm.Genre, cm.t_film_genre.ID_genre == cm.Genre.genre_id
+#         ).join(
+#             cm.t_film_country, cm.t_film_country.ID_film == cm.Film.film_id
+#         ).join(
+#             cm.Country, cm.t_film_country.ID_Country == cm.Country.country_id
+#         ).group_by( cm.t_film_country
+#             cm.Film.film_id, cm.Film.title
+#         ).order_by(cm.Film.year).all()
+#         print(result)
+#     return render_template("dbtest.html", test2=result)
+
+@app.route('/dbtest2')
+def dbtest2():
+    with app.app_context():
+        query = select(
+            cm.Film.title,
+            cm.Film.year,
+            cm.Film.director,
+            cm.Film.duration_mins,
+            cm.Film.rating,
+            func.group_concat(func.distinct(cm.Country.country_name).op('SEPARATOR')(', ')).label('countries'),
+            func.group_concat(func.distinct(cm.Genre.genre_name).op('SEPARATOR')(', ')).label('genres')
+        ).select_from(
+            cm.Film
+        ).outerjoin(
+            cm.Film.country
+        ).outerjoin(
+            cm.Film.genre
+        ).group_by(
+            cm.Film.film_id
+        ).order_by(cm.Film.title)
+
+        result = db.session.execute(query).all()
+    return render_template("dbtest.html", test2=result)
+
+
+# @app.route('/dbtest')
+# def dbtest():
+#     with app.app_context():
+#         result = db.session.query(
+#             Users.name, Users.surname, Users.email, Reviews.content
+#             ).join(
+#             Reviews, Users.user_ID == Reviews.ID_User
+#             ).all()
+#     return render_template("dbtest.html", list = result)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from typing import Optional
+import datetime
+import decimal
 
-from sqlalchemy import ForeignKeyConstraint, Index, String
+from sqlalchemy import Column, DECIMAL, DateTime, ForeignKeyConstraint, Index, String, Table
 from sqlalchemy.dialects.mysql import INTEGER
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
@@ -8,26 +10,135 @@ class Base(DeclarativeBase):
     pass
 
 
-class Users(Base):
-    __tablename__ = 'users'
+class Country(Base):
+    __tablename__ = 'country'
 
-    user_ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
-    name: Mapped[str] = mapped_column(String(30), nullable=False)
-    surname: Mapped[str] = mapped_column(String(30), nullable=False)
-    email: Mapped[Optional[str]] = mapped_column(String(50))
+    country_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    country_name: Mapped[str] = mapped_column(String(100), nullable=False)
 
-    reviews: Mapped[list['Reviews']] = relationship('Reviews', back_populates='users')
+    film: Mapped[list['Film']] = relationship('Film', secondary='film_country', back_populates='country')
 
 
-class Reviews(Base):
-    __tablename__ = 'reviews'
+class Film(Base):
+    __tablename__ = 'film'
+
+    film_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    year: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
+    director: Mapped[str] = mapped_column(String(100), nullable=False)
+    duration_mins: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
+    description: Mapped[str] = mapped_column(String(1000), nullable=False)
+    rating: Mapped[Optional[decimal.Decimal]] = mapped_column(DECIMAL(10, 1))
+
+    country: Mapped[list['Country']] = relationship('Country', secondary='film_country', back_populates='film')
+    genre: Mapped[list['Genre']] = relationship('Genre', secondary='film_genre', back_populates='film')
+    session: Mapped[list['Session']] = relationship('Session', back_populates='film')
+
+
+class Genre(Base):
+    __tablename__ = 'genre'
+
+    genre_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    genre_name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    film: Mapped[list['Film']] = relationship('Film', secondary='film_genre', back_populates='genre')
+
+
+class Hall(Base):
+    __tablename__ = 'hall'
+
+    hall_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    row_count: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
+    places_in_row: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
+    hall_name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    session: Mapped[list['Session']] = relationship('Session', back_populates='hall')
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    user_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    login: Mapped[str] = mapped_column(String(100), nullable=False)
+    hashPassword: Mapped[str] = mapped_column(String(255), nullable=False)
+    role: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    ticket: Mapped[list['Ticket']] = relationship('Ticket', back_populates='user')
+
+
+t_film_country = Table(
+    'film_country', Base.metadata,
+    Column('ID_country', INTEGER(11), primary_key=True),
+    Column('ID_film', INTEGER(11), primary_key=True),
+    ForeignKeyConstraint(['ID_country'], ['country.country_id'], ondelete='CASCADE', onupdate='CASCADE', name='film_country_ibfk_1'),
+    ForeignKeyConstraint(['ID_film'], ['film.film_id'], ondelete='CASCADE', onupdate='CASCADE', name='film_country_ibfk_2'),
+    Index('ID_film', 'ID_film'),
+    Index('film_country_pkfk', 'ID_country', 'ID_film')
+)
+
+
+
+t_film_genre = Table(
+    'film_genre', Base.metadata,
+    Column('ID_genre', INTEGER(11), primary_key=True),
+    Column('ID_film', INTEGER(11), primary_key=True),
+    ForeignKeyConstraint(['ID_film'], ['film.film_id'], ondelete='CASCADE', onupdate='CASCADE', name='film_genre_ibfk_1'),
+    ForeignKeyConstraint(['ID_genre'], ['genre.genre_id'], ondelete='CASCADE', onupdate='CASCADE', name='film_genre_ibfk_2'),
+    Index('ID_film', 'ID_film'),
+    Index('film_genre_pkfk', 'ID_genre', 'ID_film')
+)
+
+
+class Session(Base):
+    __tablename__ = 'session'
     __table_args__ = (
-        ForeignKeyConstraint(['ID_User'], ['users.user_ID'], ondelete='CASCADE', onupdate='CASCADE', name='reviews_ibfk_1'),
-        Index('review_user_fk', 'ID_User')
+        ForeignKeyConstraint(['ID_film'], ['film.film_id'], ondelete='CASCADE', onupdate='CASCADE', name='session_ibfk_1'),
+        ForeignKeyConstraint(['ID_hall'], ['hall.hall_id'], ondelete='CASCADE', onupdate='CASCADE', name='session_ibfk_2'),
+        Index('session_film_fk', 'ID_film'),
+        Index('session_hall_fk', 'ID_hall')
     )
 
-    review_ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
-    ID_User: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
-    content: Mapped[str] = mapped_column(String(500), nullable=False)
+    session_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    session_datetime: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False)
+    price: Mapped[decimal.Decimal] = mapped_column(DECIMAL(10, 0), nullable=False)
+    ID_film: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
+    ID_hall: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
 
-    users: Mapped['Users'] = relationship('Users', back_populates='reviews')
+    film: Mapped['Film'] = relationship('Film', back_populates='session')
+    hall: Mapped['Hall'] = relationship('Hall', back_populates='session')
+    seat: Mapped[list['Seat']] = relationship('Seat', back_populates='session')
+    ticket: Mapped[list['Ticket']] = relationship('Ticket', back_populates='session')
+
+
+class Seat(Base):
+    __tablename__ = 'seat'
+    __table_args__ = (
+        ForeignKeyConstraint(['ID_session'], ['session.session_id'], ondelete='CASCADE', onupdate='CASCADE', name='seat_ibfk_1'),
+        Index('seat_session_fk', 'ID_session')
+    )
+
+    seat_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    row: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
+    place: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
+    status: Mapped[str] = mapped_column(String(50), nullable=False)
+    ID_session: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
+
+    session: Mapped['Session'] = relationship('Session', back_populates='seat')
+
+
+class Ticket(Base):
+    __tablename__ = 'ticket'
+    __table_args__ = (
+        ForeignKeyConstraint(['ID_session'], ['session.session_id'], ondelete='CASCADE', onupdate='CASCADE', name='ticket_ibfk_1'),
+        ForeignKeyConstraint(['ID_user'], ['user.user_id'], ondelete='CASCADE', onupdate='CASCADE', name='ticket_ibfk_2'),
+        Index('ticket_session_fk', 'ID_session'),
+        Index('ticket_user_fk', 'ID_user')
+    )
+
+    ticket_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    purchase_datetime: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False)
+    ID_session: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
+    ID_user: Mapped[int] = mapped_column(INTEGER(11), nullable=False)
+
+    session: Mapped['Session'] = relationship('Session', back_populates='ticket')
+    user: Mapped['User'] = relationship('User', back_populates='ticket')
